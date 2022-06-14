a6c13c175571dc53773cedb2e0e0be803b0752d8
Maks <maksimaikido@mail.ru>
2022-06-14 23:27:11 +0500

    added LOL

--- main.c
+++ main.c
@@ -2,6 +2,10 @@
 
 int main(int argc, char * argv[])
 {
+<<<<<<< HEAD
 	printf("hello world NIGGERS NIGGERSSSSSS\n");
+=======
+	printf("hello world LOL LOL LOL\n");
+>>>>>>> b61f2ad (added LOL)
 	return 0;
 }
